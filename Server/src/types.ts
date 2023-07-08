4ea8750117773f4be406c659c3bd4e59a1ba0e72
Backwards <AlexHardingYT@gmail.com>
2023-07-08 07:44:26 -0400

    Combined Types into one folder and placed it into src

---
+++ Server/src/types.ts
@@ -0,0 +1,28 @@
+// Not -required- for the code, but does help keep things clear.
+type EventSender = "CLIENT"
+    | "SERVER";
+
+// Again not too important, but does help keep things readable.
+type EventType = "SEND"
+    | "REQUEST"
+
+// This is the important bit of the Event, it is how the code deciphers what is needed.
+type EventDemand = "VERSION"
+    | "MESSAGE";
+
+type Content = string
+    | number
+    | object
+    | string[]
+    | number[]
+    | object[]
+
+type Version = `${number}:${number}:${number}`
+
+export {
+    EventSender,
+    EventType,
+    EventDemand,
+    Content,
+    Version
+}
